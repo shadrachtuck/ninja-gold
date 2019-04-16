2ff286773c8353e5b933b711e9e0df75adf51b0d
Shadrach Tuck <shad.tuck@yahoo.com>
2019-04-16 13:46:58 -0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'public';
+  title = 'Ninja Gold';
+  total;
+
+  constructor(){
+    this.total = 0;
+  }
+    addGold() {
+      this.total++;
+    }
+
+    reset(){
+      this.total = 0;
+    }
+
+    farm(min,max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return this.total += Math.floor(Math.random() * (20 - 10 + 1)) + 10;
+    }
+    cave(min,max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return this.total += Math.floor(Math.random() * (10 - 5 + 1)) + 5;
+    }
+    house(min,max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return this.total += Math.floor(Math.random() * (10 - 5 + 1)) + 5;
+    }
+    casino(min,max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return this.total += Math.floor(Math.random() * (100 - 5 + 1)) + 5;
+    }
 }
